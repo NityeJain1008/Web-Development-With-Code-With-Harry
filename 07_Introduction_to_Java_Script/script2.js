68b2b1d3f5534bc17c99571b314ab736b5956fba
Nitye Jain <jnitye@gmail.com>
2026-07-11 23:21:33 +0530

    Add files via upload

---
+++ 07_Introduction_to_Java_Script/script2.js
@@ -0,0 +1,17 @@
+console.log("I am a variable") ;
+
+let a = "Nitye" ;
+console.log(a) ; 
+let b = 65 ; 
+console.log(b) ; 
+b = 100 ; 
+console.log(b) ; 
+
+console.log(typeof "Hello"); // string
+console.log(typeof 100); // number
+console.log(typeof true); // boolean
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object (quirk!)
+console.log(typeof {}); // object
+console.log(typeof []); // object (array is also object )
+console.log(typeof function(){});// function
