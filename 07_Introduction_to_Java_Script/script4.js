aa3e58570d1b5f9309ae6b9a648778a323b800ed
Nitye Jain <jnitye@gmail.com>
2026-07-13 22:23:29 +0530

    Add files via upload

---
+++ 07_Introduction_to_Java_Script/script4.js
@@ -0,0 +1,14 @@
+let a = 45 ; 
+let b = 43 ; 
+
+console.log("Operators in JS")
+console.log("a X b =", a*b) ; 
+console.log("a + b =", a+b) ; 
+console.log("a - b =", a-b) ; 
+console.log("a / b =", a/b) ; 
+console.log("a ^ b =", a**b) ; 
+console.log("remainder", a%b) ;  
+
+let age = 17;
+let result = age >= 18 ? "Adult" : "Minor";
+console.log(result); // "Minor"
