73575bcbf31f52defcceb66fde8add5246d8fefb
Nitye Jain <jnitye@gmail.com>
2026-07-19 20:30:02 +0530

    Add files via upload

---
+++ 07_Introduction_to_Java_Script/script9.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+    let c = a + b;
+    return c;
+}
+
+function greet(name) {
+    console.log("welcome", name);
+}
+
+const sayHi = function () {
+    console.log("Hi there!");
+};
+
+const square = (num) => {
+    return num * num;
+};
+
+console.log(add(5, 6));
+
+let a = "Nitye";
+greet(a);
+
