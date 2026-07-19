8b4012ff39c0f450bff7565e098b113b7e06ac39
Nitye Jain <jnitye@gmail.com>
2026-07-19 20:30:24 +0530

    Add files via upload

---
+++ 07_Introduction_to_Java_Script/script10.js
@@ -0,0 +1,20 @@
+let arr = [1, 2, 3, 4, "java"] ; 
+console.log(arr[4]) ; 
+console.log(arr.length) ; 
+
+
+arr[3] = 9 ; 
+console.log(arr) ; 
+
+let student = [
+    {name : "Harry", class : 10},
+    {name : "nitye", class : 9},
+    {Name : "devansh", class : 8},
+    {name : "Anmol", class : 10}
+]
+
+console.log(student[3]) ; 
+console.log(student[3].name) ; 
+console.log(student[3].class) ; 
+
+console.log(student.length) ; 
