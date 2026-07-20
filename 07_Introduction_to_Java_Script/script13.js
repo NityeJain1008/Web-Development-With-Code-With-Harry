0ca196813cd06914fc5d27eb4e2431ff20d181a8
Nitye Jain <jnitye@gmail.com>
2026-07-20 22:44:00 +0530

    Add files via upload

---
+++ 07_Introduction_to_Java_Script/script13.js
@@ -0,0 +1,12 @@
+let name = "Nitye" ; 
+let cgpa = "9.48" ; 
+
+let str = `${name} is a good boy and got a cgpa of ${cgpa}` ; 
+
+console.log(str) ; 
+
+let a = 45 ; 
+let b = 66 ; 
+
+let sum = `sum of a and b is ${a+b}`;
+console.log(sum) ; 
