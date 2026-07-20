9643d7a9c618d4a3ad87d8b99522f5cb1fa58872
Nitye Jain <jnitye@gmail.com>
2026-07-20 22:43:27 +0530

    Add files via upload

---
+++ 07_Introduction_to_Java_Script/script12.js
@@ -0,0 +1,14 @@
+let name = "Nitye Jain\nis a software developer" ; 
+let name1 = 'harry \nis a good boy' ; 
+
+let poem = `twinke Twinkle Little Star
+                    by - Author name
+`
+
+console.log(name1) ; 
+console.log(name) ; 
+console.log(poem) ; 
+
+name1 = "New name" ; 
+
+console.log("this is name1", name1) ; 
