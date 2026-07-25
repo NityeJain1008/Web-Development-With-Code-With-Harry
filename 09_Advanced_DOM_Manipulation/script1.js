b0f6ff6cd0cad74236c34965b8707f992c8414b2
Nitye Jain <jnitye@gmail.com>
2026-07-25 21:22:16 +0530

    Add files via upload

---
+++ 09_Advanced_DOM_Manipulation/script1.js
@@ -0,0 +1,21 @@
+console.log("Hello") 
+
+const welcome = document.getElementById("welcome") 
+welcome.textContent = "WELCOME" ; 
+welcome.style.backgroundColor = "red" 
+
+const input = document.getElementById("username") ; 
+input.value = "Type your name" ; 
+
+console.log(input.value) ; 
+
+const cont = document.querySelector(".container") ; 
+// console.log(cont.value)
+
+
+document.body.style.display = "flex";
+document.body.style.justifyContent = "center";
+document.body.style.alignItems = "center";
+// document.body.style.height = "100vh";
+
+
