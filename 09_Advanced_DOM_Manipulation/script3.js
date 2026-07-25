4341e9fbce46ce16ac68e9889d23931f2df16edb
Nitye Jain <jnitye@gmail.com>
2026-07-25 21:22:47 +0530

    Add files via upload

---
+++ 09_Advanced_DOM_Manipulation/script3.js
@@ -0,0 +1,5 @@
+const newDiv = document.createElement("div");
+newDiv.textContent = "Hello, I was created dynamically!";
+newDiv.setAttribute("class", "newDiv")
+newDiv.innerText = "hellp"
+container.append(newDiv) ; 
