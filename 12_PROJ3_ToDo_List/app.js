526a453d94addb60efb894571e2a3a496daabf23
Nitye Jain <jnitye@gmail.com>
2026-08-04 22:42:46 +0530

    Add files via upload

---
+++ 12_PROJ3_ToDo_List/app.js
@@ -0,0 +1,40 @@
+const addTodoBtn = document.getElementById("addTodoBtn") ; 
+let inputTag = document.getElementById("todoInput") ;
+let todoListUl = document.getElementById("todoList") ; 
+let todoText ; 
+let todos = [] ; 
+let todosString = localStorage.getItem("todos") ; 
+
+if(todosString){
+    todos = JSON.parse(todosString) ; 
+}
+
+const populateTodos = ()=>{
+    let string = "" ;
+    for(const todo of todos){
+        string += `<li class="todo-item ${todo.isCompleted? "completed":""}">
+                    <input type="checkbox" class="todo-checkbox" ${todo.isCompleted? "checked":""} >
+                    <span class="todo-text">${todo.title}</span>
+                    <button class="delete-btn">×</button>
+                  </li>`
+    }
+    todoListUl.innerHTML =  todoListUl.innerHTML + string ; 
+    // console.log(todo.isCompleted? "checked":"") ; 
+}
+
+
+addTodoBtn.addEventListener("click", ()=>{
+    console.log("clicked") ; 
+    todoText = inputTag.value ; 
+    console.log(todoText) ; 
+    inputTag.value = "" ; 
+    let todo = {
+        title : todoText,
+        isCompleted : false 
+    }
+    todos.push(todo) ; 
+    localStorage.setItem("todos", JSON.stringify(todos)) ; 
+
+})
+
+populateTodos() ; 
