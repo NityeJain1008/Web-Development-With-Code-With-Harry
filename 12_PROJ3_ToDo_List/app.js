1e774b76a3f13231ec2373724e4aca98b8e1e982
Nitye Jain <jnitye@gmail.com>
2026-08-06 21:52:36 +0530

    Update app.js

--- 12_PROJ3_ToDo_List/app.js
+++ 12_PROJ3_ToDo_List/app.js
@@ -12,15 +12,16 @@ if(todosString){
 const populateTodos = ()=>{
     let string = "" ;
     for(const todo of todos){
-        string += `<li class="todo-item ${todo.isCompleted? "completed":""}">
+        string += `<li id="todo-${todo.id}" class="todo-item ${todo.isCompleted? "completed":""}">
                     <input type="checkbox" class="todo-checkbox" ${todo.isCompleted? "checked":""} >
                     <span class="todo-text">${todo.title}</span>
                     <button class="delete-btn">×</button>
                   </li>`
     }
-    todoListUl.innerHTML =  todoListUl.innerHTML + string ; 
+    todoListUl.innerHTML =  string ; 
     // console.log(todo.isCompleted? "checked":"") ; 
 }
+
 
 addTodoBtn.addEventListener("click", ()=>{
     console.log("clicked") ; 
@@ -28,12 +29,15 @@ addTodoBtn.addEventListener("click", ()=>{
     console.log(todoText) ; 
     inputTag.value = "" ; 
     let todo = {
+        id: todos.length,
         title : todoText,
         isCompleted : false 
     }
     todos.push(todo) ; 
     localStorage.setItem("todos", JSON.stringify(todos)) ; 
-
+    populateTodos() ; 
+    
 })
 
 populateTodos() ; 
+const todoCheckboxes = document.querySelectorAll(".todo-checkbox")
