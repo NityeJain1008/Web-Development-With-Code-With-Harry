432a10f8f91c8c95ab3840df5c87a7a3647801fb
Nitye Jain <jnitye@gmail.com>
2026-08-12 23:02:30 +0530

    Add files via upload

---
+++ 13_Node/03_Modules in Nodejs/app.js
@@ -0,0 +1,10 @@
+let math = require("./math") ; 
+let greet = require("./greeting") ; 
+
+console.log(math.add(1, 2)) ; 
+
+console.log(math.sub(8, 6)) ; 
+
+console.log(math.mult(4,5)) ;
+
+greet.greet() ; 
