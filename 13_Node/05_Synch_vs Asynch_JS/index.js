b5a6686c2005c6e5d4111373fd36e9ae6466dcff
Nitye Jain <jnitye@gmail.com>
2026-08-14 22:47:50 +0530

    Add files via upload

---
+++ 13_Node/05_Synch_vs Asynch_JS/index.js
@@ -0,0 +1,11 @@
+console.log("A") ; 
+setTimeout(()=>{
+    console.log("B") ; 
+}, 100)
+
+console.log("C") ; 
+console.log("D") ; 
+console.log("E") ; 
+console.log("F") ; 
+console.log("G") ; 
+console.log("H") ; 
