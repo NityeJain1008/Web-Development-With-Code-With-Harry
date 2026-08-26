620547f84fee4b2851974bc24c82e565bddc4f8d
Nitye Jain <jnitye@gmail.com>
2026-08-26 23:13:13 +0530

    Add files via upload

---
+++ 13_Node/08_Async Await in JS/index.js
@@ -0,0 +1,16 @@
+function waitTwoSeconds() {
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            resolve("Waited for 2 seconds");
+        }, 2000);
+    });
+}
+
+async function runTask() {
+    console.log("Start");
+    const result = await waitTwoSeconds();
+    console.log(result);
+    console.log("End");
+}
+
+runTask();
