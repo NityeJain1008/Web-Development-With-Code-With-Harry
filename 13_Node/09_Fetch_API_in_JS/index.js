7c1f0a0154c46345c57cc6f4494cbc0031c70267
Nitye Jain <jnitye@gmail.com>
2026-08-29 22:45:15 +0530

    Add files via upload

---
+++ 13_Node/09_Fetch_API_in_JS/index.js
@@ -0,0 +1,10 @@
+const url = "https://jsonplaceholder.typicode.com/posts/1" ; 
+
+const data = fetch(url) ; 
+
+data.then((value)=>{
+    return value.json() ;
+}).then((response)=>{
+    console.log(response) ; 
+})
+
